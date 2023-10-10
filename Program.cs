0e2a44bfc01a9b6b8da876def61b93b810195a73
Moloko Sehata <moloko4@live.com>
2023-10-10 12:48:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Static_Methods_and_Classes
+{
+    static internal class useFullTools
+    {
+        public static void sayHi(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            useFullTools.sayHi("Moloko");
+            Console.ReadLine();
+        }
+    }
+}
